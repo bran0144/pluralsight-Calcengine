990e7de5cab7997d488ef3b5465ff1b34ebd2b40
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-02-25 13:11:13 -0600

    added method overloads

--- src/com/pluralsight/calcengine/MathEquation.java
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -25,6 +25,19 @@ public class MathEquation {
         this.leftVal = leftVal;
         this.rightVal = rightVal;
     }
+    public void execute(double leftVal, double rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+    }
+    public void execute(int leftVal, int rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+        result = (int)result;
+    }
     public void execute() {
         switch (opCode) {
             case 'a':
