d8c4f4beec69248f4ea77669fea56617d5bd5a1a
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-02-26 13:55:37 -0600

    refactoring with new classes

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -37,6 +37,22 @@ public class Main {
 		equationOverload.execute((double)leftInt, rightInt);
 		System.out.println("result = ");
 		System.out.println(equationOverload.getResult());
+
+		System.out.println();
+		System.out.println("Using inheritance");
+
+		CalculateBase[] calculators = {
+				new Divider(100.0d, 50.0d),
+				new Adder(25.0d, 92.0d),
+				new Subtractor(225.0d, 17.0d),
+				new Multiplier(11.0d, 3.0d)
+		};
+
+		for (CalculateBase calculator:calculators) {
+			calculator.calculate();
+			System.out.println("result=");
+			System.out.println(calculator.getResult());
+		}
 }
 
 }
